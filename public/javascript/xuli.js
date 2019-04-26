67b973d082f5a2f0821949e6700d0cabcc85865e
VukhuongDuy <vukhuongduy23@gmail.com>
2019-04-26 22:33:57 +0700

    second

--- public/javascript/xuli.js
+++ public/javascript/xuli.js
@@ -1,4 +1,4 @@
-var socket = io("http://localhost:3000");
+var socket = io("https://messagevd.herokuapp.com/");
 
 $("#btnDangNhap").click(function(){
     socket.emit('Client-send-login',$("#txtAccount").val())
